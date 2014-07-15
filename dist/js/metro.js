12dbb20ae8d68675de66034c9049ea33c11f4bd4
A-G-F <a_g_f@gmx.net>
2014-07-15 19:36:58 +0200

    add blog

--- dist/js/metro.js
+++ dist/js/metro.js
@@ -9,8 +9,8 @@
 if (typeof jQuery === "undefined") { throw new Error("Metro UI 4 Jekyll requires jQuery") }
 
 var METRO_AUTO_REINIT;
-var METRO_LOCALE = "en"; // see possible values in metro-locale.js
-var METRO_WEEK_START = 1; // 0 - Sunday, 1 - Monday
+var METRO_LOCALE;     // see possible values in metro-locale.js
+var METRO_WEEK_START; // 0 - Sunday, 1 - Monday
 var METRO_DIALOG = false;
 
 
